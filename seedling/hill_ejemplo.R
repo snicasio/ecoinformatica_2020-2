bb01a8889a9557cdd491520d4ddc06dd491366ff
snicasio <sergio.nicasio@gmail.com>
2020-03-11 17:54:56 -0600

    Va

--- seedling/hill_ejemplo.R
+++ seedling/hill_ejemplo.R
@@ -2,7 +2,13 @@
 sd <- lapply(list.files()[grep(".csv",list.files())], read.csv)
     for(i in seq(sd)){
         sd[[i]] <- sd[[i]][,-1]
+        sd[[i]] <- sd[[i]][!rowSums(sd[[i]])==0,]
     }
 
 
-do.call(rbind,lapply(sd,colSums))
+
+todo <- do.call(rbind,lapply(sd,colSums))
+
+
+sd[[1]]/rowSums(sd[[1]])
+sd[[2]]/rowSums(sd[[2]])
