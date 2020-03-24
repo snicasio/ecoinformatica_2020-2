d13e12e56745b74a2583f825965aab96027a5228
snicasio <sergio.nicasio@gmail.com>
2020-03-23 18:30:48 -0600

    Ejemplo profe

--- sna_lineal.R
+++ sna_lineal.R
@@ -5,4 +5,32 @@ names(holi)
 
 source("loocv.R")
 
+RES <- lapply(levels(iris$Species),
+              function(x)
+                  lapply(c("Petal.Length", "Petal.Width"),
+                         function(y)
+                             do.call(rbind,lapply(names(iris)[c(-3,-5)],
+                                                  function(z)
+                                                      cv(iris[iris$Species == x,],dep = y, ind = z)))))
+names(RES) <- levels(iris$Species)
+    for(i in seq(RES))
+        for(j in seq(RES[[i]]))
+            for(k in seq(RES[[i]][[j]])){
+                names(RES[[i]]) <- c("Petal.Length", "Petal.Width")
+                rownames(RES[[i]][[j]]) <- names(iris)[c(-3,-5)]
+        
+    }
+
+
+
+
+
+
+
+
 cv(x=holi,dep = "Petal.Length", ind = "Sepal.Length")
+
+
+
+
+
