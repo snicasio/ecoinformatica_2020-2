3db277cbfd1d455a7559a314e75754f08be0f57e
snicasio <sergio.nicasio@gmail.com>
2020-03-23 18:10:22 -0600

    Función "loocv"

--- loocv.R
+++ loocv.R
@@ -5,7 +5,7 @@ cv <- function(x,dep,ind){
         val[i] <- predict(lm(paste0(dep, "~", ind) , data = x[-i,]), x)[i]
     }
     error <- mean((val-x[,dep])^2)
-    ind_mss <- (1/nrow(x))*(sum((x[,ind]-mean(x[,ind]))^2))
+    ind_mss <- (1/nrow(x))*(sum((x[,dep]-mean(x[,dep]))^2))
     Rcv <- 1-(error/ind_mss)
     R <- summary(mod)$adj.r.squared
     return(data.frame(error,R,Rcv))
